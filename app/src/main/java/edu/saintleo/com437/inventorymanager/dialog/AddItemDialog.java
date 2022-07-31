c6ae194fb43a11dca8564e32f7cd05af1ac94c5a
Jason Cidras <jcidras@me.com>
2022-07-31 15:45:47 -0400

    Updated the first screen to reflect more of the mockups. Updated add item modal to have the category dropdown.

--- app/src/main/java/edu/saintleo/com437/inventorymanager/dialog/AddItemDialog.java
+++ app/src/main/java/edu/saintleo/com437/inventorymanager/dialog/AddItemDialog.java
@@ -5,8 +5,12 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -15,19 +19,47 @@ import edu.saintleo.com437.inventorymanager.R;
 
 public class AddItemDialog extends DialogFragment {
     View dialogView;
+    Spinner dropdown;
+    String selectedItem;
+    String[] items;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // fetch the layout inflater
         LayoutInflater inflater = requireActivity().getLayoutInflater();
+        // inflate the dialog view
         dialogView = inflater.inflate(R.layout.dialog_add_item, null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(dialogView)
-                .setPositiveButton(R.string.dialog_add, (dialogInterface, i) -> {
+        // get the dropdown control
+        dropdown = dialogView.findViewById(R.id.ddl_item_category);
+        // populate the dropdown
+        items = getResources().getStringArray(R.array.categories);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getContext(),
+                android.R.layout.simple_spinner_item,
+                items);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedItem = items[i];
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        // create builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // build the view
+        builder.setView(dialogView)
+                .setTitle(R.string.dialog_add_item_title)
+                .setPositiveButton(R.string.dialog_add, (dialogInterface, i) -> {
                     EditText txtItemName = dialogView.findViewById(R.id.txt_item_name);
+                    String message = String.format("Item Name: %s, Item Category: %s", txtItemName.getText(), selectedItem);
                     Snackbar.make(
                             requireActivity().findViewById(R.id.nav_host_fragment_content_main),
-                            txtItemName.getText().toString(),
+                            message,
                             Snackbar.LENGTH_SHORT).show();
                 })
                 .setNegativeButton(R.string.dialog_cancel, (dialogInterface, i) -> {
